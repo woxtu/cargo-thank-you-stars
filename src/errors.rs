ec7d8a81fad8df53d3f27153e7b95e9f4878bbfa
woxtu <woxtup@gmail.com>
2017-08-29 01:50:58 +0900

    Ask crate data to crates.io

--- src/errors.rs
+++ src/errors.rs
@@ -1,10 +1,12 @@
 use std::io;
 
+use reqwest;
 use toml;
 
 error_chain! {
   foreign_links {
     Io(io::Error);
+    Reqwest(reqwest::Error);
     Toml(toml::de::Error);
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate error_chain;
+extern crate reqwest;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
@@ -9,6 +10,7 @@ mod errors;
 mod lockfile;
 
 use std::env;
+use std::io::Read;
 
 use errors::*;
 
@@ -22,7 +24,10 @@ quick_main!(|| -> Result<()> {
 
   for dependency in lockfile.root.dependencies {
     if dependency.is_registry() {
-      println!("{}", dependency.crate_id())
+      let mut contents = String::new();
+      let _ = reqwest::get(&format!("https://crates.io/api/v1/crates/{}", dependency.crate_id()))?
+        .read_to_string(&mut contents)?;
+      println!("{}", contents);
     }
   }
 
