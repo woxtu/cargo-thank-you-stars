b87e4d9e1f5833ccaea328429c59149e5ffb8b48
Michal Piotrowski <michal@idiomatic-rust.com>
2022-04-22 21:37:23 +0000

    update

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,13 @@
+use crate::errors::*;
+use serde::Deserialize;
 use std::fs;
 use std::path::Path;
-use serde::Deserialize;
-use crate::errors::*;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-  pub token: String,
+    pub token: String,
 }
 
 pub fn read(path: &Path) -> Result<Config> {
-  Ok(json::from_str(&fs::read_to_string(&path)?)?)
+    Ok(json::from_str(&fs::read_to_string(&path)?)?)
 }
--- src/github.rs
+++ src/github.rs
@@ -1,14 +1,14 @@
-use reqwest::header::AUTHORIZATION;
 use crate::errors::*;
+use reqwest::header::AUTHORIZATION;
 
 pub fn star(token: &str, repository: &str) -> Result<()> {
-  let _ = reqwest::Client::new()
-    .put(&format!(
-      "https://api.github.com/user/starred/{}",
-      repository
-    ))
-    .header(AUTHORIZATION, format!("token {}", token))
-    .send()?;
+    let _ = reqwest::Client::new()
+        .put(&format!(
+            "https://api.github.com/user/starred/{}",
+            repository
+        ))
+        .header(AUTHORIZATION, format!("token {}", token))
+        .send()?;
 
-  Ok(())
+    Ok(())
 }
